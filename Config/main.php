6b01e5d8e4a55a4465dcffb8433d67199298ddf7
Digit <digit.vova@gmail.com>
2018-04-16 12:47:48 +0300

    Add basic config for docker container

--- Config/main.php
+++ Config/main.php
@@ -1,3 +1,23 @@
 <?php
 //Main config for all application
-return [];
+return [
+    //Application config
+    'app' => [
+        'charset' => 'utf-8',
+        'hostname' => '0.0.0.0',    //For docker - expose to all interfaces
+        'port' => 4000,
+        //Initial app aliases
+        'aliases' => [],
+        //Components
+        'components' => [],
+    ],
+    //DI definitions
+    'container' => [
+
+    ],
+    //DI config
+    'container.config' => [
+        'useAnnotations' => false,
+        'useAutowiring' => true,
+    ],
+];
