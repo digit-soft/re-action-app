3e00618ab20a48c1c9da514a6c9d65814aa5fb2e
digit <digit.vova@gmail.com>
2018-05-11 15:05:17 +0300

    Default SiteController changes

--- Controllers/SiteController.php
+++ Controllers/SiteController.php
@@ -7,8 +7,8 @@ use Reaction\Annotations\Ctrl;
 use Reaction\Annotations\CtrlAction;
 use Reaction\Helpers\Url;
 use Reaction\Promise\Promise;
+use Reaction\RequestApplicationInterface;
 use Reaction\Routes\Controller;
-use Reaction\Web\AppRequestInterface;
 use Reaction\Web\Response;
 
 /**
@@ -23,34 +23,35 @@ class SiteController extends Controller
      * @CtrlAction(path="/test")
      * @CtrlAuth
      * Index action
-     * @param AppRequestInterface $request
+     * @param RequestApplicationInterface $app
      * @return Promise
      */
-    public function actionIndex(AppRequestInterface $request)
+    public function actionIndex(RequestApplicationInterface $app)
     {
-        return new Promise(function ($r, $c) use ($request) {
+        return new Promise(function ($r, $c) use ($app) {
             try {
+                $request = $app->reqHelper;
                 $sessionKey = date('Y_m_d__H_i');
-                $request->session->set($sessionKey, time());
+                $app->session->set($sessionKey, time());
 
                 $data = [
-                    'getQuery' => $request->queryString,
-                    'getQueryParams' => $request->queryParams,
-                    'getCookies' => $request->cookies,
-                    //'getHeaders' => $request->headers,
-                    'getContentType' => $request->contentType,
-                    'getAbsoluteUrl' => $request->absoluteUrl,
-                    'getUrl' => $request->url,
-                    'getPathInfo' => $request->pathInfo,
-                    'getScriptFile' => $request->scriptFile,
-                    'getScriptUrl' => $request->scriptUrl,
+                    'getQuery' => $app->reqHelper->getQueryString(),
+                    'getQueryParams' => $request->getQueryParams(),
+                    'getCookies' => $request->getCookies(),
+                    //'getHeaders' => $request->getHeaders(),
+                    'getContentType' => $request->getContentType(),
+                    'getAbsoluteUrl' => $request->getAbsoluteUrl(),
+                    'getUrl' => $request->getUrl(),
+                    'getPathInfo' => $request->getPathInfo(),
+                    'getScriptFile' => $request->getScriptFile(),
+                    'getScriptUrl' => $request->getScriptUrl(),
                 ];
                 $body = print_r($data, true);
-                $body .= "\n" . print_r($request->response->cookies, true);
-                $body .= "\n" . print_r($request->session->data, true);
+                $body .= "\n" . print_r($app->response->cookies, true);
+                $body .= "\n" . print_r($app->session->data, true);
 
-                $request->response->setBody($body);
-                $response = $request->response->build();
+                $app->response->setBody($body);
+                $response = $app->response->build();
                 $r($response);
                 return;
             } catch (\Throwable $e) {
@@ -63,26 +64,27 @@ class SiteController extends Controller
 
     /**
      * @CtrlAction(path="/test1/test2/{name:\w+}")
-     * @param AppRequestInterface $request
+     * @param RequestApplicationInterface $app
      * @return \Reaction\Web\ResponseBuilderInterface
      */
-    public function actionTest(AppRequestInterface $request)
+    public function actionTest(RequestApplicationInterface $app)
     {
-        $request->response->setBody('Test');
-        return $request->response;
+        $app->response->setBody('Test');
+        return $app->response;
     }
 
     /**
      * @CtrlAction(path="/test2/{name:\w+}[/{id:\d+}]", method={"GET","POST"})
-     * @param AppRequestInterface $request
+     * @param RequestApplicationInterface $app
+     * @param string                      $name
      * @return \Reaction\Web\ResponseBuilderInterface
      */
-    public function actionTest2(AppRequestInterface $request, $name)
+    public function actionTest2(RequestApplicationInterface $app, $name)
     {
         $args = func_get_args();
         array_shift($args);
-        $args[] = Url::current([], false, $request);
-        $request->response->setBody(print_r($args, true));
-        return $request->response;
+        $args[] = Url::current([], false, $app);
+        $app->response->setBody(print_r($args, true));
+        return $app->response;
     }
 }
