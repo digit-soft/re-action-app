17b63a4f71255fd22af6b7c8a9f189ffbaf378dd
digit <digit.vova@gmail.com>
2018-06-07 12:49:57 +0300

    Add index page view file

---
+++ Views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @var \Reaction\Web\View $this
+ */
+
+$this->title = 'Home';
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center"><?= $this->title ?></h1>
+        </div>
+    </div>
+</div>
